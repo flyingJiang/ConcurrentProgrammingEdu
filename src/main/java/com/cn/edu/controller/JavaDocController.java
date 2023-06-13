11893d802c7c213bc2be103728b9766b0188eb04
jiangjianfei <jiangjianfei@baidu.com>
2023-06-13 14:52:26 +0800

    javadoc注释

---
+++ src/main/java/com/cn/edu/controller/JavaDocController.java
@@ -0,0 +1,22 @@
+package com.cn.edu.controller;
+
+/**
+ * @program: ConcurrentProgrammingEdu
+ * @description:
+ * @author: jiangjianfei
+ * @create: 2023-06-13 14:41
+ **/
+public class JavaDocController {
+    /**
+     * 测试JavaDoc
+     *
+     * @param key 关键字
+     * @param value 值
+     * @return 关键字+值
+     */
+    public String doJavaDoc(String key, String value) {
+        // zh_CN
+        // -encoding UTF-8 -charset UTF-8
+        return key + value;
+    }
+}
